760ec0c8e17bf7db0dba35d42fb7f02770784e42
Pedro-LM1 <pedro.lucasmf1@gmail.com>
2025-03-20 15:51:23 +0000

    Nivel aventureiro completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,46 +1,101 @@
 #include <stdio.h>
 
+#define TAMANHO 10
+#define NAVIO 3
+
 
 int main(){
-    int tabuleiro[10][10];
+    int tabuleiro[TAMANHO][TAMANHO];
 
-
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+    // dá valor 0 em todo o tabuleiro
+    for(int i = 0; i < TAMANHO; i++){
+        for(int j = 0; j < TAMANHO; j++){
             tabuleiro[i][j] = 0;
         }
     }
 
-    // vetores dos barcos
-    int barco1[3] = {3,3,3};
-    int barco2[3] = {3,3,3};
+    // Vetor do navio
+    int barco[NAVIO] = {NAVIO, NAVIO, NAVIO};
 
-    // confere se o barco ultrapassa a coluna
+
+    // Horizontal - verifica se ultrapassa linha e se sobrepõe
     int linha1 = 2, coluna1 = 3;
-    if(coluna1 + 2 < 10){
-        for(int h = 0; h < 3; h++){
-            tabuleiro[linha1][coluna1 + h] = barco1[h];
+    if(coluna1 + NAVIO - 1 < TAMANHO){
+        int podeColocar = 1;
+        for(int h = 0; h < NAVIO; h++){
+            if(tabuleiro[linha1][coluna1 + h] != 0){
+                podeColocar = 0;
+                break;
+            }
+        }
+        if(podeColocar){
+            for(int i = 0; i < NAVIO; i++){
+                tabuleiro[linha1][coluna1 + i] = barco[i];
+            }
         }
     }
 
-    // confere se o barco ultrapassa a linha
+    // Vertical - se ultrapassa coluna e se sobrepõe
     int linha2 = 5, coluna2 = 6;
-    if(linha2 + 2 < 10){
-        for(int v = 0; v < 3; v++){
-            tabuleiro[linha2 + v][coluna2] = barco2[v];
+    if(linha2 + NAVIO - 1 < TAMANHO){
+        int podeColocar = 1;
+        for(int v = 0; v < NAVIO; v++){
+            if(tabuleiro[linha2 + v][coluna2] != 0){
+                podeColocar = 0;
+                break;
+            }
+        }
+        if(podeColocar){
+            for(int i = 0; i < NAVIO; i++){
+                tabuleiro[linha2 + i][coluna2] = barco[i];
+            }
         }
     }
 
-    printf("\n");
-    printf(" ");
-    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    for(int c = 0; c < 10; c++){
+    // Diagonal esquerda-baixo - verifica se ultrapassa linha, coluna e se sobrepõe
+    int linha3 = 0, coluna3 = 9;
+    if(linha3 + NAVIO - 1 < TAMANHO && coluna3 - (NAVIO - 1) >= 0){
+        int podeColocar = 1;
+        for(int i = 0; i < NAVIO; i++){
+            if(tabuleiro[linha3 + i][coluna3 - i] != 0){
+                podeColocar = 0;
+                break;
+            }
+        }
+        if(podeColocar){
+            for(int i = 0; i < NAVIO; i++){
+                tabuleiro[linha3 + i][coluna3 - i] = barco[i];
+            }
+        }
+    }
+
+    // Diagonal direita-cima - verifica se ultrapassa linha, coluna e se sobrepõe
+    int linha4 = 8, coluna4 = 1;
+    if(linha4 - (NAVIO - 1) >= 0 && coluna4 + NAVIO - 1 < TAMANHO){
+        int podeColocar = 1;
+        for(int i = 0; i < NAVIO; i++){
+            if(tabuleiro[linha4 - i][coluna4 + i] != 0){
+                podeColocar = 0;
+                break;
+            }
+        }
+        if(podeColocar){
+            for(int i = 0; i < NAVIO; i++){
+                tabuleiro[linha4 - i][coluna4 + i] = barco[i];
+            }
+        }
+    }
+
+    // ====== TABULEIRO =======
+    printf("\n  ");
+    char linha[TAMANHO] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    for(int c = 0; c < TAMANHO; c++){
         printf(" %c", linha[c]);
     }
-     
-    for(int i = 0; i < 10; i++){
-        printf("\n%2i ", i+1);
-        for(int j = 0; j < 10; j++){
+    
+    for(int i = 0; i < TAMANHO; i++){
+        printf("\n%2i ", i + 1);
+        for(int j = 0; j < TAMANHO; j++){
             printf("%i ", tabuleiro[i][j]);
         }
     }
